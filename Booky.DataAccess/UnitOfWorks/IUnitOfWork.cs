b662b32888c9b46216e64a8ba20bf4c58a3cb0d1
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-11-13 00:18:40 +0500

    domain

---
+++ Booky.DataAccess/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Booky.DataAccess.Repositories;
+using Booky.Domain.Entities;
+
+namespace Booky.DataAccess.UnitOfWorks;
+
+public interface IUnitOfWork : IDisposable
+{
+
+    IRepository<Author> Authors { get; }
+    IRepository<Book> Books { get; }
+    IRepository<Publisher> Publishers { get; }
+    IRepository<Review> Reviews { get; }
+
+    IRepository<BookAuthor> Book_Authors { get; }
+
+    ValueTask<bool> SaveAsync();
+}
---
+++ Booky.DataAccess/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using Booky.DataAccess.Contexts;
+using Booky.DataAccess.Repositories;
+using Booky.Domain.Entities;
+
+namespace Booky.DataAccess.UnitOfWorks;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly BookyDbContext context;
+
+    public UnitOfWork(BookyDbContext context)
+    {
+        this.context = context;
+
+        Authors = new Repository<Author>(context);
+        Books = new Repository<Book>(context);
+        Publishers = new Repository<Publisher>(context);
+        Reviews = new Repository<Review>(context);
+        Book_Authors = new Repository<BookAuthor>(context);
+    }
+
+
+    public IRepository<Author> Authors { get; }
+
+    public IRepository<Book> Books { get; }
+
+    public IRepository<Publisher> Publishers { get; }
+
+    public IRepository<Review> Reviews { get; }
+
+    public IRepository<BookAuthor> Book_Authors { get; }
+
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    public async ValueTask<bool> SaveAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+}
